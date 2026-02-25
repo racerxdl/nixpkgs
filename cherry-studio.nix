c9b74e3155ed6df48160f37c5e866b57bb01f0a6
Lucas Teske <lucas@teske.com.br>
2026-02-25 01:05:46 -0300

    Add flake.nix

--- cherry-studio.nix
+++ cherry-studio.nix
@@ -1,11 +1,15 @@
-{ lib, appimageTools, fetchurl }:
+{
+  lib,
+  appimageTools,
+  fetchurl,
+}:
 let
   version = "1.7.19";
   pname = "cherry-studio";
   src = fetchurl {
     url = "https://github.com/CherryHQ/cherry-studio/releases/download/v${version}/Cherry-Studio-${version}-x86_64.AppImage";
     # Keep the hash you calculated earlier
-    hash = "sha256-z23Rqt0qAM0JtoMWwy2XCwnDjFo+3nB+706lK0ZrWaw="; 
+    hash = "sha256-z23Rqt0qAM0JtoMWwy2XCwnDjFo+3nB+706lK0ZrWaw=";
   };
   appimageContents = appimageTools.extractType2 { inherit pname version src; };
 in
--- flake.nix
+++ flake.nix
@@ -1,19 +1,24 @@
-
 {
   description = "RXDL Custom Packages";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs, ... }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    # This defines the packages output
-    packages.${system} = {
-      cherry-studio = pkgs.callPackage ./cherry-studio.nix { };
-      # When you add a new package, just add a line here:
-      # my-other-app = pkgs.callPackage ./my-other-app.nix { };
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      # This defines the packages output
+      packages.${system} = {
+        cherry-studio = pkgs.callPackage ./cherry-studio.nix { };
+        # When you add a new package, just add a line here:
+        # my-other-app = pkgs.callPackage ./my-other-app.nix { };
+      };
+
+      overlays.default = final: prev: {
+        cherry-studio = prev.callPackage ./cherry-studio.nix { };
+      };
     };
-  };
 }
